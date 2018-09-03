ac89b464817e21e7d09a765322af55deae6dd94c
Theodor-Felix Iacob <theodor.iacob@armada-it.net>
2018-09-03 10:39:00 +0300

    federation address migration typo

--- services/db/db-files/migrations_src/00005_federation_server.sql
+++ services/db/db-files/migrations_src/00005_federation_server.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
-ALTER TABLE user_wallet RENAME COLUMN federation_server TO friendly_id;
+ALTER TABLE user_wallet RENAME COLUMN federation_address TO friendly_id;
 ALTER TABLE user_wallet ADD COLUMN domain character varying(255) NOT NULL DEFAULT '';
 
 DROP INDEX IF EXISTS idx_user_wallet_fedname;
@@ -9,7 +9,7 @@ CREATE unique INDEX idx_user_wallet_fedname ON user_wallet(friendly_id,domain) w
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE user_wallet RENAME COLUMN friendly_id TO federation_server;
+ALTER TABLE user_wallet RENAME COLUMN friendly_id TO federation_address;
 ALTER TABLE user_wallet DROP COLUMN domain;
 
 DROP INDEX IF EXISTS idx_user_wallet_fedname;
