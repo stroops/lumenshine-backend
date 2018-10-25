2c5f85648e900d089c34c87b349e688e5e9fc446
Gaudi Cristian <cristian.gaudi@armada-it.net>
2018-10-25 14:45:38 +0300

    Improvement sql statement

--- services/db/db-files/migrations_src/00012_walletorder_update.sql
+++ services/db/db-files/migrations_src/00012_walletorder_update.sql
@@ -1,15 +1,10 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
-WITH Numbered AS
-(
-    SELECT id, user_id, order_nr, ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY id) as rownum
-    FROM user_wallet
-)
-
 UPDATE user_wallet
-SET order_nr = rownum - 1
-FROM Numbered WHERE user_wallet.id = Numbered.id;
+SET order_nr = indexer.rownum - 1
+FROM (SELECT id, ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY id) as rownum FROM user_wallet) indexer
+WHERE user_wallet.id = indexer.id;
 
 
 -- +goose Down
